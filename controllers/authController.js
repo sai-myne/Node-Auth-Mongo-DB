e59067894f4ad99c67deaf50d5d823b8e54ef647
sai-myne <ahlusir@gmail.com>
2020-09-05 10:27:15 +0800

    Testing Routes & Handling POST Requests

--- controllers/authController.js
+++ controllers/authController.js
@@ -12,5 +12,7 @@ module.exports.signup_post = (req, res) => {
 }
 
 module.exports.login_post = (req, res) => {
+    const { email, password } = req.body;
+    console.log(email, password);
     res.send('user login');
 }
