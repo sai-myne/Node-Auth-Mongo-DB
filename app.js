cf5c8a0b5db10fcf1369d6afe8b8a1337199847a
sai-myne <52918521+sai-myne@users.noreply.github.com>
2020-09-06 12:04:45 +0800

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.set('view engine', 'ejs');
 
 // database connection
-const dbURI = 'mongodb+srv://saiMyne:test1234@nodeauth.edowt.mongodb.net/node-auth';
+const dbURI = '';
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
   .then((result) => app.listen(3000))
   .catch((err) => console.log(err));
